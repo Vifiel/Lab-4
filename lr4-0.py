17938e9b1c778cf0e6a31460e5fd8f0f97d30e4b
Vifiel <ibaklanov.696@gmail.com>
2024-12-25 17:18:26 +0300

    Job done

--- lr4-0.py
+++ lr4-0.py
@@ -1,48 +1,43 @@
-class Item:
-    def __init__(self, weight, value):
-        self.weight = weight
-        self.value = value
+
+back = [[0]*3 for i in range(3)]
+score = 15
+
+things3 = [["r", 25], ["x", 20]]
+things2 = sorted([["p", 15], ["a", 15], ["s", 20], ["c", 20]], key = lambda x: x[1], reverse=1)
+things1 = sorted([["i", 100500], ["k", 15], ["t", 25], ["f", 15], ["d", 10]], key = lambda x: x[1], reverse=1)
+score = 0
+
+def place(r):
+    global score
+    max1 = things3[0][1]
+    max2 = things2[0][1] + things1[0][1]
+    max3 = sum([i[1] for i in things1][:2])
+    if max1 > max2 and max1 > max3:
+        back[r] = [things3[0][0]*3]
+        things3.pop(0)
+        score += max1
+    elif max2 > max3:
+        back[r][0] = things1[0][0]
+        back[r][1] = things2[0][0]
+        back[r][2] = things2[0][0]
+        things1.pop(0)
+        things2.pop(0)
+        score += max2
+    else:
+        back[r][0] = things1[0][0]
+        back[r][1] = things1[1][0]
+        back[r][2] = things1[2][0]
+        for j in range(3):
+            things1.pop(0)
+        score += max3
 
 
-def knapsack(items, capacity):
-    def node_bound_count(i, weight, value):
-        if weight > capacity:
-            return 0
-        node_value = value
-        j = i
-        total_weight = weight
 
-        while j < len(items) and total_weight + items[j].weight <= capacity:
-            node_value += items[j].value
-            total_weight += items[j].weight
-            j += 1
-        if j < len(items):
-            node_value += (capacity - total_weight) * (items[j].value / items[j].weight)
-
-        return node_value
-    
-
-    def branch_bound(i, weight, value):
-        nonlocal max_value
-        if weight <= capacity and value > max_value:
-            max_value = value
-        if i == len(items):
-            return 0
-        if node_bound_count(i, weight, value) > max_value:
-            branch_bound(i+1, weight, value)
-        if value + (capacity - weight) * (items[i].value / items[i].weight) > max_value:
-            branch_bound(i+1, weight + items[i].weight, value + items[i].value)
-
-
-    items = sorted(items, key=lambda x: x.value / x.weight, reverse=True)
-    max_value = 0
-    branch_bound(0, 0, 0)
-    return max_value
-
-
-if __name__ == '__main__':
-    items = [Item(3, 700), Item(1, 300), Item(2, 500),
-             Item(2, 550), Item(3, 800)]
-    capacity = 4
-    max_value = knapsack(items, capacity)
-    print(max_value)
+if __name__ == "__main__":
+    for i in range(3):
+        place(i)
+    left1 = sum([i[1] for i in things1])
+    left2 = sum([i[1] for i in things2])
+    left3 = sum([i[1] for i in things3])
+    score = score - 100500 + 5 - left1 - left2 - left3
+    print(back, score)
